9cdd0d14bbb15cea9920e0965dba8eb2c4e47419
Tetiana Sobko <sobkotiana@gmail.com>
2025-04-14 14:50:48 +0300

    Fix flake8 issues

--- cinema/urls.py
+++ cinema/urls.py
@@ -9,7 +9,9 @@ router.register("genres", GenreViewSet, basename="genre")
 router.register("actors", ActorViewSet, basename="actor")
 router.register("cinema_halls", CinemaHallViewSet, basename="cinema_hall")
 router.register("movies", MovieViewSet, basename="movie")
-router.register("movie_sessions", MovieSessionViewSet, basename="movie_session")
+router.register(
+    "movie_sessions", MovieSessionViewSet, basename="movie_session"
+)
 
 urlpatterns = [
     path("", include(router.urls)),
